d5306329e7e27abbdf35efc80921ffc001436781
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2023-02-17 16:03:57 +0100

    "updated"

--- demo/admin.py
+++ demo/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import House, Room
 
-house = House.objects.create(name='_4000')
-Room.objects.create(house=house)
+# house = House.objects.create(name='_4000')
+# Room.objects.create(house=house)
 
 
 class HouseAdmin(admin.ModelAdmin):
